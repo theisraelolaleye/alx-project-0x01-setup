0bc127f7c594c064e835551be788c06dd3077dfd
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-17 08:19:41 +0100

    usercard created

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { UserProps } from '@/interfaces';
+import UserCard from '@/components/common/UserCard';
 
 const UsersPage = () => {
+  const users: UserProps[] = []; // Fetch or pass users data here
+
   return (
-    <div>UsersPage</div>
+    <div>
+      {users.map(user => (
+        <UserCard key={user.id} {...user} />
+      ))}
+    </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
 
 export default UsersPage
