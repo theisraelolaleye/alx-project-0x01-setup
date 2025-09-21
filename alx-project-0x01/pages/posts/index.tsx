78de4adbabfab79c5cfedd9eff512848e070461e
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-21 23:03:21 +0100

    4. More Components (Extending Post Page)

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,18 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostProps, PostData } from "@/interfaces";
+import { useState } from "react";
 
 const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
-  console.log(posts)
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -20,6 +29,10 @@ const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
           }
         </div>
       </main>
+
+      {
+        isModalOpen && <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      }
     </div>
   )
 }
